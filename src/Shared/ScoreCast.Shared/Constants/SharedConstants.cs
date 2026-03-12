9a5daa0e430fe0783d7cb84f679a4371ecd0ad86
Nitesh Parajuli <parajulinitesh22@gmail.com>
2026-03-12 16:59:17 -0400

    fixes

--- src/Shared/ScoreCast.Shared/Constants/SharedConstants.cs
+++ src/Shared/ScoreCast.Shared/Constants/SharedConstants.cs
@@ -4,4 +4,5 @@ public static class SharedConstants
 {
     public static readonly string Any = "ANY";
     public static readonly string ApiIdentifier = "ScoreCast.Ws";
+    public static readonly string DefaultSchema = "scorecast";
 }
