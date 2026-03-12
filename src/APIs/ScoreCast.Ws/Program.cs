91d6292f30ee763c5a9052b246f67b2ecee83ba2
Nitesh Parajuli <parajulinitesh22@gmail.com>
2026-03-12 14:27:49 -0400

    Configure WebService: Serilog, FastEndpoints versioning, Swagger, CORS, DbContext, global exception handler

--- src/APIs/ScoreCast.Ws/Program.cs
+++ src/APIs/ScoreCast.Ws/Program.cs
@@ -1,12 +1,84 @@
-using FastEndpoints;
-using FastEndpoints.Swagger;
+using System.Diagnostics;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddFastEndpoints();
-builder.Services.SwaggerDocument();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-var app = builder.Build();
-app.UseFastEndpoints();
-app.UseSwaggerGen();
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
 
-app.Run();
+    if (builder.Environment.IsEnvironment("Local"))
+        builder.Configuration.AddUserSecrets<Program>();
+
+    Log.Information("Starting {ApplicationName} API", builder.Environment.ApplicationName);
+
+    builder.Host.UseSerilog((context, configuration) =>
+        configuration.ReadFrom.Configuration(context.Configuration));
+
+    builder.Services.AddFastEndpoints(opt =>
+    {
+        opt.Assemblies =
+        [
+            typeof(EndpointsGroup).Assembly,
+            typeof(InfrastructureGroup).Assembly
+        ];
+    });
+
+    builder.AddApiCommonServices();
+    builder.Services.AddScoreCastInfrastructure(builder.Environment.EnvironmentName);
+
+    builder.Services.AddApiVersioning(o =>
+    {
+        o.DefaultApiVersion = new ApiVersion(1, 0);
+        o.AssumeDefaultVersionWhenUnspecified = true;
+        o.ApiVersionReader = ApiVersionReader.Combine(
+            new UrlSegmentApiVersionReader(),
+            new QueryStringApiVersionReader("v"),
+            new HeaderApiVersionReader("x-api-version"));
+        o.ReportApiVersions = true;
+        o.ApiVersionSelector = new CurrentImplementationApiVersionSelector(o);
+    });
+
+    builder.Services.SwaggerDocument(opt =>
+    {
+        opt.DocumentSettings = ds =>
+        {
+            ds.Title = "ScoreCast APIs";
+            ds.Description = "ScoreCast Premier League Predictions API";
+            ds.Version = "v1";
+            ds.MarkNonNullablePropsAsRequired();
+        };
+        opt.ReleaseVersion = 1;
+        opt.AutoTagPathSegmentIndex = 0;
+        opt.EnableGetRequestsWithBody = false;
+        opt.EnableJWTBearerAuth = !(builder.Environment.IsProduction() || builder.Environment.IsStaging());
+        opt.ShortSchemaNames = true;
+        opt.SerializerSettings = s =>
+        {
+            s.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+            s.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+            s.PropertyNameCaseInsensitive = true;
+            s.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+        };
+    });
+
+    var app = builder.Build();
+
+    app.ConfigureScoreCastMiddlewares();
+
+    await app.RunAsync();
+}
+catch (Exception ex)
+{
+    if (Debugger.IsAttached)
+    {
+        Console.WriteLine(ex);
+        Debug.WriteLine(ex);
+    }
+    Log.Fatal(ex, "Application terminated unexpectedly - {Message}", ex.GetBaseException().Message);
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
