bdff81ecf0916ce8c81accd0f9edd79346432ae0
Nitesh Parajuli <parajulinitesh22@gmail.com>
2026-03-14 04:14:58 -0400

    feat: Redirect authenticated users to /leagues, update predict route and back navigation

--- src/Web/ScoreCast.Web/Pages/Home.razor.cs
+++ src/Web/ScoreCast.Web/Pages/Home.razor.cs
@@ -1,7 +1,21 @@
+using Microsoft.AspNetCore.Components.Authorization;
+
 namespace ScoreCast.Web.Pages;
 
 public partial class Home
 {
     [CascadingParameter(Name = "IsDarkMode")]
     private bool IsDarkMode { get; set; }
+
+    [CascadingParameter]
+    private Task<AuthenticationState> AuthState { get; set; } = default!;
+
+    [Inject] private NavigationManager Nav { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        var state = await AuthState;
+        if (state.User.Identity?.IsAuthenticated == true)
+            Nav.NavigateTo("/leagues", replace: true);
+    }
 }
