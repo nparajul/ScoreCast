64ec6e872d6d16065a6350b9865f8842e7d32777
Nitesh Parajuli <nparajul@ramapo.edu>
2026-03-16 16:46:09 -0400

    Show insights for current + next gameweek scheduled matches

--- src/APIs/ScoreCast.Ws.Endpoints/V1/Insights/GetMatchInsightsEndpoint.cs
+++ src/APIs/ScoreCast.Ws.Endpoints/V1/Insights/GetMatchInsightsEndpoint.cs
@@ -25,9 +25,9 @@ public sealed class GetMatchInsightsEndpoint
         var matches = await db.Matches
             .AsNoTracking()
             .Where(m => m.Gameweek.SeasonId == req.SeasonId
-                        && m.Gameweek.Number == req.GameweekNumber
+                        && (m.Gameweek.Number == req.GameweekNumber || m.Gameweek.Number == req.GameweekNumber + 1)
                         && m.Status == MatchStatus.Scheduled)
-            .OrderBy(m => m.KickoffTime)
+            .OrderBy(m => m.Gameweek.Number).ThenBy(m => m.KickoffTime)
             .Select(m => new
             {
                 m.Id, m.KickoffTime,
